0457383d109afa24b84091a1823b004bca03aba5
dwoloszin <3h2so4@gmail.comgit>
2023-08-08 11:19:58 -0300

    udates

--- MAIN.py
+++ MAIN.py
@@ -1,13 +1,9 @@
 from getFileFromWeb import download_csv_files
 
 if __name__ == "__main__":
-  base_url = "http://10.192.12.62/"
-  folder_path = "nbulkload/"
-  prefix = "dump_tpt_cellsector_"
-  save_path = "import/nbulkload"  # Change this to your desired save path
-  download_csv_files(base_url, folder_path, prefix, save_path)
+  download_csv_files()
 
-  
+
 
 
 
--- getFileFromWeb.py
+++ getFileFromWeb.py
@@ -1,8 +1,14 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import util
+credentials = util.getCredentials()
 
-def download_csv_files(base_url, folder_path, prefix, save_path):
+def download_csv_files():
+    base_url = credentials['base_url']
+    folder_path = "nbulkload/"
+    prefix = "dump_tpt_cellsector_"
+    save_path = "import/nbulkload"  # Change this to your desired save path
     # Create the save directory if it doesn't exist
     if not os.path.exists(save_path):
         os.makedirs(save_path)
@@ -44,9 +50,6 @@ def download_csv_files(base_url, folder_path, prefix, save_path):
             print(f"Removed older file: {oldest_file}")
 
 if __name__ == "__main__":
-    base_url = "http://10.192.12.62/"
-    folder_path = "nbulkload/"
-    prefix = "dump_tpt_cellsector_"
-    save_path = "import/nbulkload"  # Change this to your desired save path
+    
 
-    download_csv_files(base_url, folder_path, prefix, save_path)
+    download_csv_files()
